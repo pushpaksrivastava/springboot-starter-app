3bafaa9623c401a639e0c00e2c41e7d4842ddde2
Pushpak Srivastava <pushpaksrivastava01@gmail.com>
2020-11-30 21:35:42 +0530

    Login Controller Modified

--- src/main/java/com/example/springboot/web/firstspringbootdemo/controller/LoginController.java
+++ src/main/java/com/example/springboot/web/firstspringbootdemo/controller/LoginController.java
@@ -1,13 +1,23 @@
 package com.example.springboot.web.firstspringbootdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+//@RequestParam String name, ModelMap model
+	public String Welcome(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
+		return "welcome";
+	}
 
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String LoginMessage() {
 		return "login";
 	}
+
 }
--- src/main/webapp/WEBINF/jsp/login.jsp
+++ src/main/webapp/WEBINF/jsp/login.jsp
@@ -3,6 +3,12 @@
 <title>First Web Application</title>
 </head>
 <body>
-My First JSP!!
+<form action="" method="post">
+	Name: <input type="text" name="name">
+	Password: <input type="password" name="password">
+ 	<input type ="submit">
+</form>
+
+
 </body>
 </html>
