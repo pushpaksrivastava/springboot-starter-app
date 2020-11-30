c2d505cb96d0b1020c33476944c83abb0890efdd
Pushpak Srivastava <pushpaksrivastava01@gmail.com>
2020-11-30 17:35:00 +0530

    initial commit

---
+++ src/main/java/com/example/springboot/web/firstspringbootdemo/FirstSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.web.firstspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootDemoApplication.class, args);
+	}
+
+}
