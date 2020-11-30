c268304df8f59686ff50818be4c5f46bd804dc19
Pushpak Srivastava <pushpaksrivastava01@gmail.com>
2020-11-30 20:08:03 +0530

    "Login Controller"

---
+++ src/main/java/com/example/springboot/web/firstspringbootdemo/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.springboot.web.firstspringbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	@RequestMapping("/login")
+
+	public String LoginMessage() {
+		return "login";
+	}
+}
